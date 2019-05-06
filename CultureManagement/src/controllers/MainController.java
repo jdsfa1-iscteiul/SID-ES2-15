07afda39f4ca6c16ac9ea4ee655b0190bb4f874a
jdsfa1-iscteiul <jdsfa1@iscte-iul.pt>
2019-05-07 00:08:11 +0100

    FXML created

---
+++ CultureManagement/src/controllers/MainController.java
@@ -0,0 +1,15 @@
+package controllers;
+
+public class MainController {
+
+	
+	public void handleAddButton() {}
+	
+	public void handleEditButton() {}
+	
+	public void handleFiltrarButton() {}
+	
+	public void handleLoginButton() {}
+	
+	public void displaySelected() {}
+}
