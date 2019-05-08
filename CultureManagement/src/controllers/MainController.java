df3d2d98bb91c4524465062b6addd3ddfcaea9be
jdsfa1-iscteiul <jdsfa1@iscte-iul.pt>
2019-05-08 23:25:54 +0100

    addMeasurement and adminMenu templates and controllers added.

--- CultureManagement/src/controllers/MainController.java
+++ CultureManagement/src/controllers/MainController.java
@@ -1,16 +1,24 @@
 package controllers;
 
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import javafx.beans.binding.Bindings;
 import javafx.beans.binding.IntegerBinding;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.ObservableSet;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
 import utilities.Measurement;
 
 public class MainController {
@@ -53,13 +61,48 @@ public class MainController {
 		
 	}
 
-	public void handleAddCultureButton() {}
+	public void handleAddCultureButton() {
+		load_add_measurement_scene();
+	}
 	
 	public void handleEditButton() {}
 	
-	public void handleAdminMenuButton() {}
+	public void handleAdminMenuButton() {
+		
+		load_admin_menu();
+	}
 	
 	public void handleFiltrarButton() {}
 	
 	public void displaySelected() {}
+	
+	public void load_add_measurement_scene() {
+		FXMLLoader Loader = new FXMLLoader();
+		Loader.setLocation(getClass().getResource("../FXMLFiles/addMeasurement.fxml"));
+		try {
+			Loader.load();
+		} catch (IOException e) {
+			Logger.getLogger(MainController.class.getName()).log(Level.SEVERE, null, e);
+		}
+		Parent p = Loader.getRoot();
+		Stage stage = new Stage();
+		stage.setScene(new Scene(p));
+		stage.setTitle("Adicionar medição");
+		stage.show();
+	}
+	
+	public void load_admin_menu() {
+		FXMLLoader Loader = new FXMLLoader();
+		Loader.setLocation(getClass().getResource("../FXMLFiles/adminMenu.fxml"));
+		try {
+			Loader.load();
+		} catch (IOException e) {
+			Logger.getLogger(MainController.class.getName()).log(Level.SEVERE, null, e);
+		}
+		Parent p = Loader.getRoot();
+		Stage stage = new Stage();
+		stage.setScene(new Scene(p));
+		stage.setTitle("Admin menu");
+		stage.show();
+	}
 }
