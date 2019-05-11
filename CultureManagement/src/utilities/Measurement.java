4175f7effe101221ea177b4700da15dbc2ddb22e
Marco Domingues <mavcd@iscte-iul.pt>
2019-05-11 06:13:09 +0100

    [em desenvolvimento] implementação parcial da classe Measurement

--- CultureManagement/src/utilities/Measurement.java
+++ CultureManagement/src/utilities/Measurement.java
@@ -2,6 +2,30 @@ package utilities;
 
 public class Measurement {
 	
+	private String variableName, timestamp;
 	
+	private float valueMeasured;
+	
+	public Measurement(String variableName, String timestamp, String valueMeasured) {
+		this.variableName = variableName;
+		this.timestamp = timestamp;
+		this.valueMeasured = Float.parseFloat(valueMeasured);
+	}
+	
+	public String getTimestamp() {
+		return timestamp;
+	}
+	
+	public float getValueMeasured() {
+		return valueMeasured;
+	}
+	
+	public String getVariableName() {
+		return variableName;
+	}	
 
+	@Override
+	public String toString() {
+		return variableName + ": { valor medido: " + valueMeasured + " }  -  " + timestamp;
+	}
 }
