d8eba4f93d39053da0ef89ecd16e63b2bd8647e5
Marco Domingues <mavcd@iscte-iul.pt>
2019-05-12 19:16:44 +0100

    Classe temporaria, irá ser apagada em breve

--- CultureManagement/src/database/mysql/ConnectionMariaDB.java
+++ CultureManagement/src/database/mysql/ConnectionMariaDB.java
@@ -1,22 +1,22 @@
 package database.mysql;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//import java.sql.SQLException;
 
 public class ConnectionMariaDB {
 	
-	Connection conn = null;
-	String dbName = "grupo15_main";
-
-	public boolean connectToDB(String user, String password) {
-		try {
-			Connection conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/" + dbName +
-																"?user=" + user + "&password=" + password);
-			
-			return true;
-		} catch (SQLException e) {
-			return false;
-		}
-	}
+//	Connection conn = null;
+//	String dbName = "grupo15_main";
+//
+//	public boolean connectToDB(String user, String password) {
+//		try {
+//			Connection conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/" + dbName +
+//																"?user=" + user + "&password=" + password);
+//			
+//			return true;
+//		} catch (SQLException e) {
+//			return false;
+//		}
+//	}
 }
