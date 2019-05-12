6b1f29699a5a87ca8ca035158db475d1c5788bf2
Marco Domingues <mavcd@iscte-iul.pt>
2019-05-12 19:16:18 +0100

    Simplificação da classe relacionada com conexao a bd

--- CultureManagement/src/database/mysql/ClientConnectionHandler.java
+++ CultureManagement/src/database/mysql/ClientConnectionHandler.java
@@ -1,40 +1,97 @@
 package database.mysql;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import utilities.Administrator;
+import utilities.Researcher;
 
 public class ClientConnectionHandler {
-	
+
 	private static ClientConnectionHandler instance = null;
-	
+
 	private Connection dbConnection;
 	
-	private String username;
-	
+	private DatabaseUser user;
+
+	//private String username;
+
+	private PreparedStatement currentStatement;
+
+	private ResultSet queryResults;
+
+	//private String accountType;
+
 	public static ClientConnectionHandler getInstance() {
 		if(instance == null)
 			instance = new ClientConnectionHandler();
 		return instance;
 	}
-	
+
 	public Connection getConnection() {
 		return dbConnection;
 	}
-	
-	public String getUsername() {
-		return username;
-	}
-	
-	public void setDbConnection(Connection dbConnection) {
+
+//	public String getUsername() {
+//		return username;
+//	}
+
+	public void setDbConnection(Connection dbConnection, String username) {
 		this.dbConnection = dbConnection;
+		try {
+			prepareStatement("Select current_role");
+			executeStatement();
+			queryResults.next();
+			if(queryResults.getString("current_role").contains("administrator_role")) 
+				user = new Administrator(username);
+			else
+				user = new Researcher(username);
+//			
+//			accountType = queryResults.getString("current_role");
+//			if(accountType==null)
+//				accountType = "admin";
+		}
+		catch (SQLException exception) {
+			exception.printStackTrace();
+		}
+
 	}
 	
-	public void setUsername(String username) {
-		this.username = username;
+	public DatabaseUser getUser() {
+		return user;
 	}
-	
+
+//	public void setUsername(String username) {
+//		this.username = username;
+//	}
+
+//	public String getAccountType() {
+//		return accountType;
+//	}
+
 	public void resetClientConnection() {
 		instance=null;
 	}
-	
+
+
+	public void prepareStatement(String sqlCommand) throws SQLException {
+		currentStatement = dbConnection.prepareStatement(sqlCommand);
+
+	}
+
+	public void executeStatement() throws SQLException {
+		currentStatement.execute();
+		queryResults = currentStatement.getResultSet();
+	}
+
+	public ResultSet getQueryResults() {
+		return queryResults;
+	}
+
+	public void resetQueryResults() {
+		queryResults = null;
+	}
 
 }
