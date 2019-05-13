2a82f764335cc19fe02d0c9abfec72b27ef9c2be
Marco Domingues <mavcd@iscte-iul.pt>
2019-05-13 13:35:34 +0100

    Listas de culturas e variáveis movidas para a classe DbUser

--- CultureManagement/src/database/mysql/DatabaseUser.java
+++ CultureManagement/src/database/mysql/DatabaseUser.java
@@ -1,17 +1,67 @@
 package database.mysql;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import utilities.Culture;
+import utilities.Variable;
+
 public abstract class DatabaseUser {
 	
 	private String username, password;
 	
+	List<Culture> cultureList = new ArrayList<Culture>();
+	
+	List<Variable> variableList = new ArrayList<Variable>();
+	
 	public DatabaseUser(String username) {
 		this.username = username;
+		try {
+			initializeCultureList();
+			initializeVariableList();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public String getUsername() {
 		return username;
 	}
 	
+	private void initializeCultureList() throws SQLException {
+		ClientConnectionHandler.getInstance().prepareStatement("CALL show_all_cultures()");
+		ClientConnectionHandler.getInstance().executeStatement();
+		ResultSet results = ClientConnectionHandler.getInstance().getQueryResults();
+		while(results.next()) 
+			cultureList.add(new Culture(results));
+	}
+
+	private void initializeVariableList() throws SQLException {
+		ClientConnectionHandler.getInstance().prepareStatement("CALL show_all_variables");
+		ClientConnectionHandler.getInstance().executeStatement();
+		ResultSet results = ClientConnectionHandler.getInstance().getQueryResults();
+		while(results.next())
+			variableList.add(new Variable(results));
+	}
+	
+	public List<Culture> getCultureList() {
+		return cultureList;
+	}
+
+	public void setCultureList(List<Culture> cultureList) {
+		this.cultureList = cultureList;
+	}
+
+	public List<Variable> getVariableList() {
+		return variableList;
+	}
+
+	public void setVariableList(List<Variable> variableList) {
+		this.variableList = variableList;
+	}
+
 	public abstract String getUserType();
 
 }
